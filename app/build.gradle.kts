a38cbad750225a9c5b17718b1be99571d85bb161
skyd0se <fitzpatrick.rinsen@gmail.com>
2025-11-13 11:50:03 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
+
+
 }
 
 android {
@@ -38,6 +41,12 @@ android {
 dependencies {
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+    val roomVersion = "2.6.0"
+
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    kapt ("androidx.room:room-compiler:$roomVersion")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -8,20 +8,39 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.lab_week_10.database.Total
+import com.example.lab_week_10.database.TotalDatabase
 import com.example.lab_week_10.viewmodels.TotalViewModel
 
 class MainActivity : AppCompatActivity() {
+    // Create an instance of the TotalDatabase
+// by lazy is used to create the database only when it's needed
+    private val db by lazy { prepareDatabase() }
     private val viewModel by lazy {
         ViewModelProvider(this)[TotalViewModel::class.java]
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize the value of the total from the database
+        initializeValueFromDatabase()
+
         prepareViewModel()
     }
     private fun updateText(total: Int) {
         findViewById<TextView>(R.id.text_total).text =
             getString(R.string.text_total, total)
+    }
+
+    // Update the value of the total in the database
+    // whenever the activity is paused
+    // This is done to ensure that the value of the total is always up to date
+    // even if the app is closed
+    override fun onPause() {
+        super.onPause()
+        db.totalDao().update(Total(ID, viewModel.total.value!!))
     }
     private fun prepareViewModel(){
         // Observe the LiveData object
@@ -33,5 +52,33 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
         }
+    }
+
+    // Create and build the TotalDatabase with the name 'total-database'
+// allowMainThreadQueries() is used to allow queries to be run on the main thread
+    // This is not recommended, but for simplicity it's used here
+    private fun prepareDatabase(): TotalDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            TotalDatabase::class.java, "total-database"
+        ).allowMainThreadQueries().build()
+    }
+// Initialize the value of the total from the database
+// If the database is empty, insert a new Total object with the value of 0
+// If the database is not empty, get the value of the total from the database
+    private fun initializeValueFromDatabase() {
+        val total = db.totalDao().getTotal(ID)
+        if (total.isEmpty()) {
+            db.totalDao().insert(Total(id = 1, total = 0))
+        } else {
+            viewModel.setTotal(total.first().total)
+        }
+    }
+
+    // The ID of the Total object in the database
+// For simplicity, we only have one Total object in the database
+// So the ID is always 1
+    companion object {
+        const val ID: Long = 1
     }
 }
